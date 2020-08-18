993369c744fd036a49d9960287de7c43bc8f3a1c
Akalo Mekonen <mekonen168@gmail.com>
2020-08-18 14:34:44 +0300

    I have imported Provider from react-redux

--- src/index.js
+++ src/index.js
@@ -3,7 +3,9 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 
-import {createStore} from 'react-redux'
+import {createStore} from 'redux'
+import {Provider} from 'react-redux'
+const store = createStore()
 
 ReactDOM.render(
   <React.StrictMode>
