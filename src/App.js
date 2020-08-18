db88c1f1ba2576780faba7dff2c9eae9cf278ad9
Akalo Mekonen <mekonen168@gmail.com>
2020-08-18 18:58:11 +0300

    I have added navbar from bootstrp

--- src/App.js
+++ src/App.js
@@ -5,7 +5,18 @@ import {changeSiteName} from './actions/changeSiteName'
 function App(props) {
   return (
     <div className="App">
-        <p onClick = {props.siteChange}>{props.siteName}</p>
+    <nav class="navbar navbar-expand-lg navbar-light bg-light">
+    <a class="navbar-brand" href="#"></a>
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+      <div class="navbar-nav">
+        <a class="nav-link active" href="#">Home</a>
+        <a class="nav-link" href="#">Users</a>
+      </div>
+    </div>
+  </nav>
     </div>
   );
 }
