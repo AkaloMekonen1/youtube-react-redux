c3d9b0c5f41980b9d78747ff8022a52a49319027
Akalo Mekonen <mekonen168@gmail.com>
2020-08-18 14:57:04 +0300

    reducer is imported and added into createStore

--- src/index.js
+++ src/index.js
@@ -2,10 +2,11 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import reducer from './components/reducer'
 
 import {createStore} from 'redux'
 import {Provider} from 'react-redux'
-const store = createStore()
+const store = createStore(reducer)
 
 ReactDOM.render(
   <Provider>
