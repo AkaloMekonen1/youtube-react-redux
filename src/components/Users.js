f88c4a82dd90d24c367d768ee555186e4713f32f
Akalo Mekonen <mekonen168@gmail.com>
2020-08-19 19:53:02 +0300

    Users component created

---
+++ src/components/Users.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+
+class Users extends Components{
+    render(){
+        return(
+            <div>
+              Users
+            </div>
+        )
+    }
+}
+export default Users
