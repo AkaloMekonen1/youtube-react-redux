aa2a8892b87c9489d4e24e17855e58baee39c22c
Akalo Mekonen <mekonen168@gmail.com>
2020-08-19 18:18:45 +0300

    I have created Route login

--- src/App.js
+++ src/App.js
@@ -22,6 +22,9 @@ class App extends Component{
             </div>
           </div>
         </nav>
+        <Router>
+          <Route path="login" component={Login} />
+        </Router>
       </div>
     );
   }
