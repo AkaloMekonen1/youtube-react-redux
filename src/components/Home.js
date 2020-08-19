27663f420b820965fd6d9ebc049d58156c078059
Akalo Mekonen <mekonen168@gmail.com>
2020-08-19 18:38:03 +0300

    I have created new folder named component and added inside it component named Home

---
+++ src/components/Home.js
@@ -0,0 +1,13 @@
+const { Component } = require("react")
+
+import React, {Component} from react-dom
+class Home extends Component{
+    render(){
+        return(
+            <div>
+              Home
+            </div>
+        )
+    }
+}
+export default Home
