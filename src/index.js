4f05d568c5de1eac417753867defcc418c04f18e
Akalo Mekonen <mekonen168@gmail.com>
2020-08-23 19:25:57 +0300

    I have arranged the file

--- src/index.js
+++ src/index.js
@@ -3,9 +3,9 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reducer from './reducer'
-
 import {createStore} from 'redux'
 import {Provider} from 'react-redux'
+
 const store = createStore(reducer)
 store.subscribe(()=>console.log(store.getState()))
 
