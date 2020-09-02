5c22fc932317c550eb9428fba9d0c62985c1cc6a
Akalo Mekonen <mekonen168@gmail.com>
2020-09-02 20:16:06 +0300

    syntex corrections

--- src/containers/Login.js
+++ src/containers/Login.js
@@ -1,14 +1,27 @@
-import React from "react";
+import React, { Component } from "react";
 import { connect } from "react-redux";
 import { changeSiteName } from "../actions/changeSiteName";
 import LoginForm from "../components/LoginForm";
-const Login = () => {
-  return (
-    <div>
-      <LoginForm />
-    </div>
-  );
-};
+class Login extends Component {
+  constructor() {
+    super();
+    this.state = {
+      msg: "This is the message"
+    }
+    setTimeout(() => {
+      this.setState({
+        msg: "This is the new message"
+      })
+    }, 2000)
+  }
+  render() {
+    return (
+      <div>
+        <LoginForm msg = {this.state.msg}/>
+      </div>
+    );
+  }
+}
 
 const mapStateToProps = (globalState) => {
   return {
