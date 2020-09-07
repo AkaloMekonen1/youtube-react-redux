88beed4240e20228822c15c531ca5c82092cb90a
Akalo Mekonen <mekonen168@gmail.com>
2020-09-07 18:12:57 +0300

    at mapStateToProps i have added sitReducer to the value of siteName

--- src/App.js
+++ src/App.js
@@ -7,11 +7,13 @@ import Users from "./containers/Users";
 import User from "./containers/User";
 import Login from "./containers/Login";
 class App extends Component {
+
   render() {
     return (
       <Router>
         <nav className="navbar navbar-expand-lg navbar-light bg-light mb-5">
           <Link to = '/'>{this.props.siteName}</Link>
+          <button onClick = { this.props.siteChange }>Change Site Name</button>
           <button
             className="navbar-toggler"
             type="button"
@@ -61,7 +63,7 @@ class App extends Component {
 
 const mapStateToProps = (globalState) => {
   return {
-    siteName: globalState.siteName,
+    siteName: globalState.siteReducer.siteName,
   };
 };
 const mapDispatchToProps = {
