30449b5760d62771d173ac68f35d0af3d61ea939
AkaloMekonen1 <mekonen168@gmail.com>
2020-09-10 08:25:54 +0300

    I have imported thunk

--- src/store.js
+++ src/store.js
@@ -1,6 +1,7 @@
 import userReducer from "./reducers/userReducer";
 import siteReducer from "./reducers/siteReducer";
 import { createStore, combineReducers } from "redux";
+import thunk from "redux-thunk"
 
 const store = createStore(
   combineReducers({
