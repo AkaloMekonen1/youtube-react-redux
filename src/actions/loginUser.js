29ffcfa789da8135dc506141cbe3bb29cd779784
AkaloMekonen1 <mekonen168@gmail.com>
2020-09-10 08:38:56 +0300

    I have added the body of the function loginUser inside async function

--- src/actions/loginUser.js
+++ src/actions/loginUser.js
@@ -1,22 +1,24 @@
 export const loginUser = (email, password) => {
     console.log(email, password)
-    const body = {
-        email,
-        password
+    return async(dispatch)=>{
+        const body = {
+            email,
+            password
+        }
+        fetch('https://reqres.in/api/login', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+             body: JSON.stringify(body)
+        })
+            .then(response => response.json())
+            .then(data => {console.log(data)
+                const isLoggedIn = (typeof data.token !== 'undefined' && data.token !== '')
+                return dispatch({
+                    type: 'LOGIN',
+                    payload: isLoggedIn
+                })});
     }
-    fetch('https://reqres.in/api/login', {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json',
-        },
-         body: JSON.stringify(body)
-    })
-        .then(response => response.json())
-        .then(data => {console.log(data)
-            const isLoggedIn = (typeof data.token !== 'undefined' && data.token !== '')
-            return {
-                type: 'LOGIN',
-                payload: isLoggedIn
-            }});
     
 }
