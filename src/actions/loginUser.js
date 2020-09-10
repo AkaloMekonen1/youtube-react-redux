b0bde3594e2ad791685f1cd41460fb2c08b650e9
AkaloMekonen1 <mekonen168@gmail.com>
2020-09-10 19:18:47 +0300

    I added the second parameter on featch into variable named option

--- src/actions/loginUser.js
+++ src/actions/loginUser.js
@@ -5,13 +5,14 @@ export const loginUser = (email, password) => {
             email,
             password
         }
-        fetch('https://reqres.in/api/login', {
+        const option = {
             method: 'POST',
             headers: {
                 'Content-Type': 'application/json',
             },
              body: JSON.stringify(body)
-        })
+        }
+        fetch('https://reqres.in/api/login',option )
             .then(response => response.json())
             .then(data => {console.log(data)
                 const isLoggedIn = (typeof data.token !== 'undefined' && data.token !== '')
