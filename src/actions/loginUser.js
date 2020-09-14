0bceb08c1f77578932a630a6946b9ac1a451c110
AkaloMekonen1 <mekonen168@gmail.com>
2020-09-14 17:46:26 +0300

    I have fixed some typo error

--- src/actions/loginUser.js
+++ src/actions/loginUser.js
@@ -1,25 +1,27 @@
 export const loginUser = (email, password) => {
-    console.log(email, password)
-    return async(dispatch)=>{
+    return async(dispatch) => {
+        console.log(email, password)
         const body = {
             email,
             password
         }
-        const option = {
+        const options = {
             method: 'POST',
             headers: {
                 'Content-Type': 'application/json',
             },
-             body: JSON.stringify(body)
+            body: JSON.stringify(body)
         }
-        fetch('https://reqres.in/api/login',option )
+        fetch('https://reqres.in/api/login', options)
             .then(response => response.json())
-            .then(data => {console.log(data)
+            .then(data => {
                 const isLoggedIn = (typeof data.token !== 'undefined' && data.token !== '')
+                console.log(data)
                 return dispatch({
                     type: 'LOGIN',
                     payload: isLoggedIn
-                })});
+                })
+            });
     }
-    
+
 }
