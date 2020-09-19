1f28a1bb1eafd7a603f2c263fe64d27586510cfe
Akalo Mekonen <mekonen168@gmail.com>
2020-09-19 15:50:01 +0300

    loginUser added to console log

--- src/reducers/userReducer.js
+++ src/reducers/userReducer.js
@@ -1,9 +1,10 @@
+import { loginUser } from "../actions/loginUser"
 
 const initState ={
     loginUser: false
 }
 const userReducer = (state = initState, action)=>{
-    console.log('user reducer')
+    console.log('user reducer',state.loginUser)
     switch(action.type){
         case 'LOGIN':
             return state = {...state, loginUser: action.payload}
