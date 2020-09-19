bc4c94bd56991c31fe266e6a9e9b165efe2198b2
Akalo Mekonen <mekonen168@gmail.com>
2020-09-19 15:41:37 +0300

    a typo error was fixed

--- src/containers/PrivateRout.js
+++ src/containers/PrivateRout.js
@@ -1,20 +1,22 @@
-import React from 'react';
-import {connect} from 'react-redux'
+import React from "react";
+import { connect } from "react-redux";
 import { Route, Redirect } from "react-router-dom";
-const PrivateRouter = ({path, component: Component, loginUser , ...rest})=>{
-    return(
-        <Route path={path} {...rest} render={(props)=>{
-            if(loginUser===true){
-                return <Component {...props} {...rest}/>
-            }
-            return <Redirect to='./Login.js'/>
-        }}/>
-    )
-}
-const mapStateToProps = (globalState)=>{
-    return {loginUser: globalState.UserReducer.loginUser}
-}
-const mapDispatchToProps = {
-   
-}
+const PrivateRouter = ({ path, component: Component, loginUser, ...rest }) => {
+  return (
+    <Route
+      path={path}
+      {...rest}
+      render={(props) => {
+        if (loginUser === true) {
+          return <Component {...props} {...rest} />;
+        }
+        return <Redirect to="./Login.js" />;
+      }}
+    />
+  );
+};
+const mapStateToProps = (globalState) => {
+  return { loginUser: globalState.userReducer.loginUser };
+};
+const mapDispatchToProps = {};
 export default connect(mapStateToProps, mapDispatchToProps)(PrivateRouter);
