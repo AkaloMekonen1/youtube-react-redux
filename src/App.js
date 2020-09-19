052138aca89b9632ffba86c5d06e4021603a907b
Akalo Mekonen <mekonen168@gmail.com>
2020-09-19 18:10:32 +0300

    I have created condition if loginUser on userReducer state is true show the button Login if it is true show button Logout

--- src/App.js
+++ src/App.js
@@ -10,10 +10,14 @@ import PrivateRouter from "./containers/PrivateRout"
 import {logoutUser} from './actions/loginUser'
 class App extends Component {
   handlerLogout = ()=>{
-    
+    return this.props.logoutUser()
   }
 
   render() {
+    let loginLink = <li className="nav-item"><NavLink className="nav-link" to="/Login"> Login </NavLink></li>
+    if(this.props.login === true){
+      loginLink = <li className="nav-item"><Link className="nav-link" to="/login" onClick={this.handlerLogout}> Logout </Link></li>
+    }
     return (
       <Router>
         <nav className="navbar navbar-expand-lg navbar-light bg-light mb-5">
@@ -34,8 +38,7 @@ class App extends Component {
               <li className="nav-item"><NavLink className="nav-link" exact to="/"> Home </NavLink></li>
               <li className="nav-item"><NavLink className="nav-link" to="/User/1"> User Number 1 </NavLink> </li>
               <li className="nav-item"><NavLink className="nav-link" to="/Users"> Users </NavLink></li>
-              <li className="nav-item"><NavLink className="nav-link" to="/Login"> Login </NavLink></li>
-              <li className="nav-item"><Link className="nav-link" to="/Login" onClick={this.handlerLogout}> Logout </Link></li>
+              {loginLink}
             </ul>
           </div>
         </nav>
@@ -53,9 +56,11 @@ class App extends Component {
 const mapStateToProps = (globalState) => {
   return {
     siteName: globalState.siteReducer.siteName,
+    login: globalState.userReducer.loginUser
   };
 };
 const mapDispatchToProps = {
   siteChange: changeSiteName,
+  logoutUser: logoutUser
 };
 export default connect(mapStateToProps, mapDispatchToProps)(App);
