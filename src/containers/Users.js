1d4630c650b8a59bed5a3c415b3124d97c791b1e
Akalo Mekonen <mekonen168@gmail.com>
2020-09-21 14:05:30 +0300

    table was added

--- src/containers/Users.js
+++ src/containers/Users.js
@@ -2,7 +2,22 @@ import React from "react";
 import { connect } from "react-redux";
 import { changeSiteName } from "../actions/changeSiteName.js";
 const Users = () => {
-  return <div>Users</div>;
+  return (<table class="table table-dark">
+  <thead>
+    <tr>
+      <th scope="col">#</th>
+      <th scope="col">First Name</th>
+      <th scope="col">Last Name</th>
+    </tr>
+  </thead>
+  <tbody>
+    <tr>
+      <th scope="row">1</th>
+      <td>Mark</td>
+      <td>Otto</td>
+    </tr>
+  </tbody>
+</table>);
 };
 const mapStateToProps = (globalState) => {
   return {
