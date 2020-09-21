3769f4f9ed6491685c13eb25f433e0f7ae3cd075
Akalo Mekonen <mekonen168@gmail.com>
2020-09-21 21:07:52 +0300

    I changed the component Users to a class based component

--- src/containers/Users.js
+++ src/containers/Users.js
@@ -1,27 +1,33 @@
-import React from "react";
+import React, { Component } from "react";
 import { connect } from "react-redux";
 import { changeSiteName } from "../actions/changeSiteName.js";
-const Users = () => {
-  return (<table className="table table-dark">
-  <thead>
-    <tr>
-      <th scope="col">#</th>
-      <th scope="col">First Name</th>
-      <th scope="col">Last Name</th>
-    </tr>
-  </thead>
-  <tbody>
-     {this.props.users((user, userIndex)=><tr>
-      <th key={userIndex} scope="row">{user.id}</th>
-      <td>{user.first_name}</td>
-      <td>{user.last_name}</td>
-    </tr>)}
-  </tbody>
-</table>);
-};
+class Users extends Component {
+  render() {
+    return (
+      <table className="table table-dark">
+        <thead>
+          <tr>
+            <th scope="col">#</th>
+            <th scope="col">First Name</th>
+            <th scope="col">Last Name</th>
+          </tr>
+        </thead>
+        <tbody>
+          {this.props.users.data.map((user, userIndex) => (
+            <tr key={userIndex}>
+              <th scope="row">{user.id}</th>
+              <td>{user.first_name}</td>
+              <td>{user.last_name}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    );
+  }
+}
 const mapStateToProps = (globalState) => {
   return {
-    users: globalState.userReducer.user,
+    users: globalState.userReducer.users,
   };
 };
 const mapDispatchToState = {
