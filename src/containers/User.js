b194651d98f0c6942d88f06bdfe1c330b7c501f5
Akalo Mekonen <mekonen168@gmail.com>
2020-09-26 14:04:12 +0300

    I have returned the compnent PrivateRouter and added to it the component Cards

--- src/containers/User.js
+++ src/containers/User.js
@@ -1,19 +1,12 @@
-import React, {Component} from "react";
-import { connect } from "react-redux";
-import Cards from "../components/Cards"
-class User extends Component {
-  render() {
-    return(
-      <div> 
-        <Cards />
-      </div>
-    )
-  }
+import React from "react";
+import Cards from "../components/Cards";
+import PrivateRouter from "./PrivateRout"
+
+const User = ()=>{
+  return(
+    <div>
+    <PrivateRouter path="/user/:userID" component={Cards} />
+    </div>
+  )
 }
-const mapStateToProps = (globalState) => {
-  return {
-  };
-};
-const mapDispatchToProps = {
-};
-export default connect(mapStateToProps, mapDispatchToProps)(User);
+export default User
